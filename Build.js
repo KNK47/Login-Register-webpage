8544d3ceffe663c1bed54a6a7bac003b9aa56127
KNK47 <kunalkadam4710@gmail.com>
2024-09-23 20:09:43 +0530

    Debugged js issue

--- Build.js
+++ Build.js
@@ -1,9 +1,9 @@
 const signUpButton=document.getElementById('signUpButton');
 const signInButton=document.getElementById('signInButton');
 const signInForm=document.getElementById('signIn');
-const signUpForm=document.getElementById('signUp');
+const signUpForm=document.getElementById('signup');
 
-signUpButton.addEventListener('click', function(){
+signUpButton.addEventListener('click',function(){
     signInForm.style.display="none";
     signUpForm.style.display="block";
 })
